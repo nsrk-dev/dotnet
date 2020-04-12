8603740e9ae3c113e2d4af3403712969d1fbb59e
Narra, Shiva R <narra_shivaramakrishna@optum.com>
2020-04-12 11:23:01 +0530

    First
    First

---
+++ SignalR_Example/1/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace TransmitData_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:57459/signalr/hubs";
+            HubConnection connection = new HubConnection(url);
+
+            try
+            {
+                var proxy = connection.CreateHubProxy("myHub");
+                proxy.On<string>("MessageReceiver", s => {
+                    Console.WriteLine(s);
+                });
+
+                connection.Start().Wait();
+                proxy.Invoke("broadcastServerTime").Wait();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Connected)
+                    connection.Stop();
+            }
+
+
+        }
+    }
+}
---
+++ SignalR_Example/1/Server/MyHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace TransmitData
+{
+    public class MyHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void BroadcastServerTime()
+        {
+            Clients.All.MessageReceiver(DateTime.Now.ToString());
+        }
+    }
+}
